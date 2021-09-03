1fe80e8943d047ac0155c3f1010be2ce2d68298a
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-09-03 03:54:00 -0700

    fix for outdated forkJoin

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,7 +13,7 @@ import {
     withLatestFrom,
     concatAll, shareReplay, throttle, throttleTime
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, interval} from 'rxjs';
+import {merge, fromEvent, Observable, concat, interval, forkJoin, pipe} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import {createHttpObservable} from '../common/util';
 import {Store} from '../common/store.service';
@@ -39,9 +39,17 @@ export class CourseComponent implements OnInit, AfterViewInit {
   ngOnInit() {
 
     this.courseId = this.route.snapshot.params['id'];
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+    const course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+    const lessons$ = this.loadLessons();
 
+    /**
+FIX for outdated rxjs forkJoin
+     */
 
+    forkJoin([
+      course$.pipe(tap(course => console.log('course ', course))),
+      lessons$.pipe(tap(lessons => console.log('lessons ', lessons)))
+    ]).subscribe();
   }
 
 
