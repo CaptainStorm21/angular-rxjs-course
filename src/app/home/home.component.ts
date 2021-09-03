d5a5a6462b55e15d3e06b01e554113d311b39f09
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-09-02 22:05:26 -0700

    last part error handling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -41,44 +41,6 @@ export class HomeComponent implements OnInit {
           tap(() => console.log("http request executed")),
           map(res => Object.values(res["payload"])),
           shareReplay(),
-          /**
-           * We're going to get here two errors getting executed and
-           * we are going to get here.The finalized method, getting twice
-           * executive as expected.
-           * If this is not the behavior that you are looking for,
-           * if you want the error handler to get executed only once instead,
-           * then what we need to do is to move the catch error block up the
-           * observable chain.
-           * So instead of doing the catch error here, the output of share
-           * replay that we are going to have this shared between the two
-           * subscriptions, we can instead take here the catch error block
-           * and move this here immediately after invoking the HTP observable.
-           *
-           * So this way, the whole observable chain is going to be bypassed
-           * and we will not get here to this mapping operation and we will
-           * not get here to the share replay.
-           * Operator, let's try this new logic we are going to have over
-           * here to the home component and we are going to see that indeed
-           * here this time around. The error handling block of catch
-           * error was only executed once, but we still have here finalize being
-           * executed twice.
-           *
-           * This is as expected because we got here two subscriptions to these
-           * observable here. So two times the observable was finished because
-           * both subscriptions were terminated.
-           * If we want finalized to be handled only once per HTP request,
-           * then we will move the finalized block here and place it before
-           * the share a replay operator.
-           * And this is one thing that is general to the catch error and
-           * to the finalize operators.
-           *
-           * home component -
-           * const courses$: Observable<Course[]>
-           */
-          // if we want 2 finanze statements
-          // finalize(() => {
-          //   console.log('Finalize Executed ...');
-          // })
 
       );
 
@@ -98,3 +60,16 @@ export class HomeComponent implements OnInit {
     }
 
 }
+
+/**
+ * We are going to head over to the home component and we're going to see that
+ * this time around, catch error was only invoked once and finalized was
+ * only executed once as expected. This means that even though these
+ * observable has two separate subscriptions, one made here by the Wegener's
+ * tab and the other made here by the advanced tab, even though there are
+ * two subscriptions we are sharing here, the execution of the HTTP observable
+ * between those two subscriptions using sharereply(). So that is why we will
+ * only have one EDP request that is either going to error out or complete.
+ * And with these we have covered the catch and refroze strategy and the
+ * finalized operator.
+ */
